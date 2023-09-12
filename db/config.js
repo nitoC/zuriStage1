95e8f016e0ba466cabd28f8e7e15d39421ec4b9d
nitoc <daren2track@gmail.com>
2023-09-12 14:22:38 +0100

    fix bug on updateuser controller

--- db/config.js
+++ db/config.js
@@ -2,31 +2,16 @@ import pg from "pg";
 import dotenv from 'dotenv';
 dotenv.config()
 
-const pass = process.env.DBPASS2;
 
-const pool = new pg.Pool({
-  //connectionString: process.env.DBLINK,
-  host:'localhost',
-  user:'postgres',
-  database:'hngbackend',
-  port:process.env.DBPORT,
-  password:pass,
- 
-})
-
-
-export default pool;
-
-
-/*const pass = process.env.DBPASS ;
+const pass = process.env.DBPASS ;
 
 const pool = new pg.Pool({
   connectionString: process.env.DBLINK,
   ssl: {
     rejectUnauthorized: false
-}
- 
+  }
+  
 })
 
 
-export default pool;*/
+export default pool;
